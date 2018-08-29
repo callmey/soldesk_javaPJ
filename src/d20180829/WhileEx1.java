47175afc638e9c31a88c5eca78580d7649ddae31
soldesk <soldesk@soldesk-PC>
2018-08-29 15:13:51 +0900

    hw 1 반복문 복습_ 20180829

--- src/d20180829/WhileEx1.java
+++ src/d20180829/WhileEx1.java
@@ -8,17 +8,45 @@ public class WhileEx1 {
 		// TODO Auto-generated method stub
 		// while문 역시 중첩이 가능함. 구구단 9단까지 출력해보세요.
 
-		int num = 1;
-		int dan = 1;
-
+	
+		int num = 2;
 		while (num <= 9) {
+			int dan = 1;
 			while (dan <= 9) {
 				System.out.println(num + " * " + dan + " = " + (num * dan));
-				num++;
 				dan++;
 			}
+			num++;
+		} // 외부 while문 end
+		
+		System.out.println("-------------------------");
 
+		
+		//1부터 10까지의 합(while)
+		int sum = 0;
+		int n = 1;
+		while(n<=10) {
+			sum += n;
+			n++;
 		}
-	}
+		
+		System.out.println(sum);
+		
+		System.out.println("-------------------------");
+		//1부터 100까지 출력(while)
+		//100부터 1까지 출력(do~while)
+		
+		int n1 = 1;
+		while(n1<=100) {
+			System.out.println(n1); // n1이 100이 되면 출력 후 n1은 101이 됨.
+			n1++; //101
+		}
+		
+		System.out.println("--------------");
+		
+	do {	
+			System.out.println(--n1);
+		} while (n1 > 1); // n1이 1이 되면 n1 출력 후 중단.
 
+	}
 }
---
+++ src/exam_01/HwNo.java
@@ -0,0 +1,105 @@
+package exam_01;
+
+import java.util.Scanner;
+
+public class HwNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(">>5번");
+		System.out.println();
+		for (int i = 1; i <= 5; i++) { // 5번
+			for (int j = 1; j <= 1 * i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("--------------------------------------");
+		System.out.println();
+
+		System.out.println(">>6번");
+		System.out.println();
+		for (int i = 1; i <= 6; i++) { // 6번
+			for (int j = 1; j <= 1 * i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+		System.out.println("--------------------------------------");
+		System.out.println();
+
+		System.out.println(">>7번");
+		System.out.println();
+		int i;
+		for (i = 1; i <= 3; i++) { // 7번
+			for (int j = 1; j <= 1 * i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("몇 줄 출력? " + (i - 1));
+
+		System.out.println("--------------------------------------");
+		System.out.println();
+
+		System.out.println(">>8번");
+		System.out.println();
+		for (i = 5; i >= 1; i--) { // 8번
+			for (int j = 1 * i; j >= 1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+
+		System.out.println("--------------------------------------");
+		System.out.println();
+
+		System.out.println(">>9번");
+		System.out.println();
+		for (i = 2; i <= 9; i++) { // 9번
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + " * " + j + " = " + (i * j));
+			}
+			System.out.println();
+		}
+
+		System.out.println("--------------------------------------");
+		System.out.println();
+
+		System.out.println(">>10번");
+		System.out.println();
+		System.out.println("구구단 2단 부터 9단까지 출력하는 코드를 작성하시오.");
+		System.out.println("출력할 단(숫자)입력: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		for (i = 1; i <= 9; i++) { // 10번
+			System.out.println(num + " * " + i + " = " + (num * i));
+		}
+
+		System.out.println("--------------------------------------");
+		System.out.println();
+
+		System.out.println(">>11번");
+		System.out.println();
+		System.out.println("1부터 입력한 숫자까지의 누적합을 출력하시오.");
+		System.out.println("숫자 입력: ");
+		int num2 = sc.nextInt();
+		int sum = 0;
+
+		for (i = 1; i < num2; i++) { // 11번
+			sum += i;
+		}
+
+		System.out.println("1부터" + num2 + "까지 합은 " + sum + "입니다.");
+
+		System.out.println("--------------------------------------");
+		System.out.println();
+
+	}
+}
