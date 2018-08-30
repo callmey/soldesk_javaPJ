d1bc52274add7b6e38e0703594f27c95b2e7a65f
dayoni <callmey@nate.com>
2018-08-31 03:35:43 +0900

    6번 화폐교환기 미완성

--- src/exam_02/HwN2.java
+++ src/exam_02/HwN2.java
@@ -8,7 +8,7 @@ public class HwN2 {
 		// TODO Auto-generated method stub
 		
 		System.out.println("> 1. 사용자로부터 입력받은 값이 3의 배수인지 아닌지 출력");
-		System.out.println("입력: ");
+		System.out.println("입력: "); 
 		Scanner sc = new Scanner(System.in);
 
 		int num = sc.nextInt();
@@ -37,6 +37,57 @@ public class HwN2 {
 		System.out.println("--------------------------------------");
 		System.out.println();
 		
+		System.out.println("3. 두개 주사위를 던졌을때 합이 4가 되는 모든 경우의수를 출력하는 프로그램을 작성하시오.");
+		
+		int[] arr1 = {1,2,3,4,5,6}; //첫번째 주사위
+		int[] arr2 ={1,2,3,4,5,6}; //두번째 주사위
+		
+		for(int i = 0; i < arr1.length; i++) { 
+			for(int j = 0; j < arr2.length; j++) {
+				if((arr1[i] + arr2[j] == 4) || (arr2[i] + arr1[j] == 4)) {
+					System.out.println(arr1[i] + " , " +  arr2[j]);
+				}
+			}
+		}
+		
+		System.out.println("--------------------------------------");
+		System.out.println();
+		
+		System.out.println("4. 구구단 3단을 옆으로 출력하기");
+
+		int dan = 3;
+		
+		for(int i=1; i<=9; i++) {
+			System.out.print(dan + " * " + i + " = " + (dan*i) + "  ");
+		}
+		
+		System.out.println("--------------------------------------");
+		System.out.println();
+		
+		System.out.println("5. 구구단 옆으로 출력하기");
+		
+		for(int i = 2; i <= 9; i++) {
+			for(int j = 1; j <= 9; j++) {
+				System.out.print(dan + " * " + i + " = " + (dan*i) + "  ");
+			}
+			System.out.println();
+		}
+		
+		
+		System.out.println("--------------------------------------");
+		System.out.println();
+		
+		/*
+		System.out.println("6. 화폐교환기 : 사용자로부터 숫자를 입력받아  화폐 몇장, 동전 몇 개로 교환가능한지 메세지를 출력하시는 코드를 작성하시오");
+		int money = sc.nextInt();
+		sub
+		
+		int[] m = new int[5];
+		
+		for(int i=0; i < m.length; i++ ) {
+			
+		}*/
+
 		
 
 	}
