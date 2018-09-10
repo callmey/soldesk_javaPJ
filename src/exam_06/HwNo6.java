b14c19a375a589b175ffa344c9e01a1b60a1800f
soldesk <soldesk@soldesk-PC>
2018-09-10 15:36:48 +0900

    ex6_20180910

---
+++ src/exam_06/HwNo6.java
@@ -0,0 +1,12 @@
+package exam_06;
+
+
+public class HwNo6{
+	
+	public static void main(String[] args){
+        // 3 일때 3개 출력 , 5일때는 5개 출력
+        Lotto lt = new Lotto(3);
+        lt.select();
+        lt.print();
+    }   
+}
