4f2c4632339f9efd9ee3fe5a64aacf6b934abbcf
soldesk <soldesk@soldesk-PC>
2018-10-02 13:23:07 +0900

    JDBC 연결 20181002

--- src/d20181002/Login.java
+++ src/d20181002/Login.java
@@ -2,6 +2,10 @@ package d20181002;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -63,9 +67,45 @@ public class Login extends JFrame implements ActionListener {
 		if(obj==jbtn1) {
 			System.out.println("로그인 버튼 눌러짐");
 			// ID, PW 읽어와서 데이터베이스에 접근 후 있는 사용자 인지 비교
+			String id = jtf1.getText();
+			String pw = jpf.getText(); // depricated. 데이터가 eclipse에 들어가 있으면 줄을 그어줌. 곧 지원이 되지 않음.
+			
+			//System.out.println("id: " + id + ", pw : " + pw );
+			
+			// db 접근해서 로그인 테이블의 정보와 비교
+			Connection conn = MakeConnection.getConnection(); // try - catch 필요없음.
+			
+			StringBuffer sb = new StringBuffer();
+			sb.append("select * from login");
+			sb.append("where id = ? and pw =? ");
+			
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			try {
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setString(1,id);
+				pstmt.setString(2, pw);
+				
+				pstmt.executeQuery();
+				
+				if(rs.next()) System.out.println("존재하는 회원입니다");
+				else System.out.println("회원가입하세요");	
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}finally {
+					try {
+						if(rs!=null) rs.close();
+						if(conn!=null) conn.close();
+			} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			} 
+				
 		} else if(obj==jbtn2) {
 			System.exit(0);
 		}
-		
 	}
 } // class end()
